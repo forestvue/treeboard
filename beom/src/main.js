99d49c87852383d85afdacc3c5ca4a728dc9f162
ParkBeomMin <club20608@gmail.com>
2019-05-25 21:50:36 +0900

    add firebase

--- beom/src/main.js
+++ beom/src/main.js
@@ -3,6 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC_bQgvZYtcQjwmmXR2I1YfyZLM7P1t9tQ",
+  authDomain: "forestvue-8424e.firebaseapp.com",
+  databaseURL: "https://forestvue-8424e.firebaseio.com",
+  projectId: "forestvue-8424e",
+  storageBucket: "forestvue-8424e.appspot.com",
+  messagingSenderId: "183393322822",
+  appId: "1:183393322822:web:fe44e34d23c90b42"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+Vue.prototype.$db = firebase.firestore();
+Vue.prototype.$auth = firebase.auth();
 
 Vue.config.productionTip = false
 
