3fa6d79457b6a60bede49984ceb27bbdc1df6e4e
ParkBeomMin <club20608@gmail.com>
2019-05-26 20:50:19 +0900

    sign in email

--- beom/src/components/SignInForm.vue
+++ beom/src/components/SignInForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <form>
-      <input type="email" name id>
-      <input type="password" name id>
-      <button></button>
+      <input v-model="email" type="email" name id>
+      <input v-model="password" type="password" name id>
+      <button @click="emailSignIn(email, password)"></button>
     </form>
     <button @click="googleSignIn">google Sign In</button>
-    <router-link to="/sign-up"></router-link>
+    <router-link to="/sign-up">sign up</router-link>
   </div>
 </template>
 
@@ -17,10 +17,10 @@ export default {
       this.$auth
         .signInWithPopup(this.$googleProvider)
         .then(result => {
-            console.log(result);
-            
+          console.log(result);
           this.$db
-            .collection("Users").doc(result.user.uid)
+            .collection("Users")
+            .doc(result.user.uid)
             .set({
               displayName: result.user.displayName,
               email: result.user.email,
@@ -32,6 +32,17 @@ export default {
             .catch(err => {
               console.log(err.message);
             });
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+    },
+    emailSignIn(email, password) {
+      this.$auth
+        .signInWithEmailAndPassword(email, password)
+        .then(result => {
+          console.log(result);
+          this.$router.replace("/dashboard");
         })
         .catch(err => {
           console.log(err.message);
